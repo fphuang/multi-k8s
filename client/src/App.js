0e8bd274bee55a034fc7afe9df32efeafeba014f
fp <huang.fp@gmail.com>
2021-09-13 09:03:16 -0500

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            React on Kubernets
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
