58436696752b69304a223a5c93369eb5a887cf93
fp <huang.fp@gmail.com>
2021-09-13 09:07:58 -0500

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            React on Kubernets
+            React - Kubernets testing
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
